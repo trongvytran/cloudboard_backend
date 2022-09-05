acf20fe6a284f5610f703b50da52ec76628d58ff
Trần Trọng Vỹ <s3764425@rmit.edu.vn>
2022-09-06 02:19:35 +0700

    Vy committed ready for Demo

--- src/billboards/entities/billboard.entity.ts
+++ src/billboards/entities/billboard.entity.ts
@@ -1,96 +1,96 @@
-import { City } from '../../cities/entities/city.entity';
-import { District } from '../../districts/entities/district.entity';
-import { Subscription } from '../../subscriptions/entities/subscription.entity';
-import { User } from '../../users/entities/user.entity';
-import { Ward } from '../../wards/entities/ward.entity';
+import {City} from '../../cities/entities/city.entity';
+import {District} from '../../districts/entities/district.entity';
+import {Subscription} from '../../subscriptions/entities/subscription.entity';
+import {User} from '../../users/entities/user.entity';
+import {Ward} from '../../wards/entities/ward.entity';
 import {
-  Entity,
-  Column,
-  PrimaryGeneratedColumn,
-  ManyToOne,
-  CreateDateColumn,
-  UpdateDateColumn,
-  Index,
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    ManyToOne,
+    CreateDateColumn,
+    UpdateDateColumn,
+    Index,
 } from 'typeorm';
-import { billboardStatusEnum } from '../enums/billboardStatus.enum';
+import {billboardStatusEnum} from '../enums/billboardStatus.enum';
 
 @Entity()
 export class Billboard {
-  @PrimaryGeneratedColumn()
-  id: number;
+    @PrimaryGeneratedColumn()
+    id: number;
 
-  @ManyToOne(() => User, (user) => user.billboards)
-  user: User;
+    @ManyToOne(() => User, (user) => user.billboards)
+    user: User;
 
-  @ManyToOne(() => City, (city) => city.billboards)
-  city: City;
+    @ManyToOne(() => City, (city) => city.billboards)
+    city: City;
 
-  @ManyToOne(() => District, (district) => district.billboards)
-  district: District;
+    @ManyToOne(() => District, (district) => district.billboards)
+    district: District;
 
-  @ManyToOne(() => Ward, (ward) => ward.billboards)
-  ward: Ward;
+    @ManyToOne(() => Ward, (ward) => ward.billboards)
+    ward: Ward;
 
-  @ManyToOne(() => Subscription, (subscription) => subscription.billboards)
-  subscription: Subscription;
+    @ManyToOne(() => Subscription, (subscription) => subscription.billboards)
+    subscription: Subscription;
 
-  @Column({ type: 'float', default: 0 })
-  lat: number;
+    @Column({type: 'float', default: 0})
+    lat: number;
 
-  @Column({ type: 'float', default: 0 })
-  long: number;
+    @Column({type: 'float', default: 0})
+    long: number;
 
-  @Column()
-  @Index()
-  name: string;
+    @Column()
+    @Index()
+    name: string;
 
-  @Column()
-  description: string;
+    @Column()
+    description: string;
 
-  @Column()
-  height: number;
+    @Column()
+    height: number;
 
-  @Column()
-  width: number;
+    @Column()
+    width: number;
 
-  @Column()
-  @Index()
-  address: string;
+    @Column()
+    @Index()
+    address: string;
 
-  @ManyToOne(() => User, (user) => user.billboards, { onDelete: 'CASCADE' })
-  likes: User;
+    @ManyToOne(() => User, (user) => user.billboards, {onDelete: 'CASCADE'})
+    likes: User;
 
-  @Column()
-  duration: string;
+    @Column()
+    duration: string;
 
-  @Column()
-  price: string;
+    @Column()
+    price: string;
 
-  @Column({ default: 0 })
-  views: number;
+    @Column({default: 0})
+    view: number;
 
-  @Column({ name: 'image_url' })
-  imageUrl: string;
+    @Column({name: 'image_url'})
+    imageUrl: string;
 
-  @Column({ name: 'video_url' })
-  videoUrl: string;
+    @Column({name: 'video_url'})
+    videoUrl: string;
 
-  @Column({
-    type: 'enum',
-    enum: billboardStatusEnum,
-    default: billboardStatusEnum.pending,
-  })
-  status: billboardStatusEnum;
+    @Column({
+        type: 'enum',
+        enum: billboardStatusEnum,
+        default: billboardStatusEnum.pending,
+    })
+    status: billboardStatusEnum;
 
-  @Column({ name: 'approved_by', default: null })
-  approvedBy: string;
+    @Column({name: 'approved_by', default: null})
+    approvedBy: string;
 
-  @CreateDateColumn({ name: 'created_at' })
-  createdAt: Date;
+    @CreateDateColumn({name: 'created_at'})
+    createdAt: Date;
 
-  @Column({ name: 'expired_at' })
-  expiredAt: Date;
+    @Column({name: 'expired_at'})
+    expiredAt: Date;
 
-  @UpdateDateColumn({ name: 'updated_at' })
-  updatedAt: Date;
+    @UpdateDateColumn({name: 'updated_at'})
+    updatedAt: Date;
 }
